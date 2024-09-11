3d2f70c72210f70da5e3bd39c06f0ad901f1bc00
arianna <arianna.copa04@gmail.com>
2024-09-11 16:34:23 +0200

    modifiche

--- Attività/Arianna Copa/Mini-Attività/JS/assets/script.js
+++ Attività/Arianna Copa/Mini-Attività/JS/assets/script.js
@@ -2,15 +2,20 @@ const elemento = document.getElementById("titolo");
 console.log(elemento.innerHTML);
 elemento.innerHTML= "Buon giovedì!";
 console.log(elemento.innerHTML);
+let i = 0;
+let r = 1;
 
 function ilmiobottone(){
-    const lamiavariabile = document.getElementsByClassName("genitore");
-    console.log(lamiavariabile);
-    const miofiglio = lamiavariabile[0].appendChild(document.createElement("p"));
-    console.log(miofiglio);
-    miofiglio.innerHTML ="sono nato";
-    miofiglio.setAttribute("class" ,"bellezza");
-    miofiglio.setAttribute("id", "primogenito");
-
-    console.log(document.querySelector(".descrizione"));
+    if(i < 1) {
+        const lamiavariabile = document.getElementsByClassName("genitore");
+        console.log(lamiavariabile);
+        const miofiglio = lamiavariabile[0].appendChild(document.createElement("p"));
+        console.log(miofiglio);
+        miofiglio.innerHTML ="sono nato";
+        miofiglio.setAttribute("class" ,"bellezza");
+        miofiglio.setAttribute("id", "primogenito");
+        
+        console.log(document.querySelector(".descrizione"));
+        i ++;
+    }
 }
