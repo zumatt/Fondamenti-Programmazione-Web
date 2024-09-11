fe86a51d3659ee608eac0bfcea59d672f6d7337a
gretalapesa <167759056+gretalapesa@users.noreply.github.com>
2024-09-11 14:22:43 +0200

    crea

---
+++ Attività/Greta Lapesa/JS/script.js
@@ -0,0 +1,12 @@
+const elemento = document.getElementById("titolo");
+console.log(elemento.innerHTML);
+elemento.innerHTML = "Buon Giovedì";
+console.log(elemento.innerHTML);
+
+const contenitore =document.getElementsByClassName("genitore");
+console.log(contenitore);
+const paragrafo = document.createElement
+
+const laMiaVariabile = document.getElementsByClassName("genitore");
+console.log(laMiaVariabile)
+laMiaVariabile[0].appendChild(document.createElement("p"));
