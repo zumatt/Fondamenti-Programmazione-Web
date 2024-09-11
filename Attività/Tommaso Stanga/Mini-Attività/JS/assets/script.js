faf3c324be9a77c5f4f93956c8378b2c1020dcfe
tommifunky <167756614+tommifunky@users.noreply.github.com>
2024-09-11 16:32:26 +0200

    mercoledi

--- Attività/Tommaso Stanga/Mini-Attività/JS/assets/script.js
+++ Attività/Tommaso Stanga/Mini-Attività/JS/assets/script.js
@@ -1,10 +1,23 @@
-let a = 0;
+const elemento = document.getElementById("titolo");
+console.log(elemento.innerHTML);
+elemento.innerHTML = "Buon giovedì!";
+console.log(elemento.innerHTML);
+let figli = 0;
 
-a+=3;
 
-if (a > 3 && a == 3) {
-    console.log("Vero");
+function ilmiobottone(){
+    if(figli > 0){
+        return
+    } else {
+        const contenitore = document.getElementsByClassName("genitore");
+        console.log(contenitore);
+        const miofiglio = contenitore[0].appendChild(document.createElement("p"));
+        miofiglio.innerHTML = "sono nato";
+        miofiglio.setAttribute("class", "bellezza");
+        miofiglio.setAttribute("id", "primogenito");
+        figli ++;
+    }
 }
-else {
-    console.log("Falso");
-}
+
+const bottoncino = document.getElementsByClassName("genitore");
+bottoncino.addEventListener("click", ilmiobottone);
