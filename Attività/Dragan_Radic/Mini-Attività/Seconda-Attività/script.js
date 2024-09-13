7072d6e3ea190678e8741b2ce1e5035138681cc0
Imdragan <dragan.radic11@icloud.com>
2024-09-13 15:14:59 +0200

    update

--- Attività/Dragan_Radic/Mini-Attività/Seconda-Attività/script.js
+++ Attività/Dragan_Radic/Mini-Attività/Seconda-Attività/script.js
@@ -1,3 +1,25 @@
-let b = ["Ciao","Ieri","Scrivania","Pennarelli"];
+const elemento = document.getElementById("titolo");
+console.log(elemento.innerHTML);
+elemento.innerHTML = "Buon gion giorno";
+console.log(elemento.innerHTML);
+let i = 0;
 
-console.log(b);
+
+
+
+
+
+function ilMioBottone(){
+    if(i < 1){
+        const laMiaVariabile = document.getElementsByClassName("genitore");
+        console.log(laMiaVariabile);
+        const mioFiglio = laMiaVariabile[0].appendChild(document.createElement("p"));
+        mioFiglio.innerHTML = "sono nato";
+        mioFiglio.setAttribute("class", "bellezza");
+        mioFiglio.setAttribute("id","primogenito");
+        i ++;
+    } 
+}
+
+const bottoncino = document.getElementById("bottone");
+bottoncino.addEventListener("click", ilMioBottone);
