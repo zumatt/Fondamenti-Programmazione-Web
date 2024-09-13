9a14db3317816137e96e1c082a9788db4f86f812
lucastocker <lucastocker777@gmail.com>
2024-09-13 14:34:21 +0200

    aggiornamenti

--- Attività/Luca Stocker/Progetto Intermedio/assets/script.js
+++ Attività/Luca Stocker/Progetto Intermedio/assets/script.js
@@ -1,8 +1,9 @@
 const tiolo = document.getElementById("titolo");
+const orario = document.getElementById("orario");
 const data = document.getElementById("data");
 
-const macDate = new Date();
-const giorniSettimana = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerd`", "Sabato","Domenica"];
+let macDate = new Date();
+const giorniSettimana = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato","Domenica"];
 const numGiorno = macDate.getDay();
 
 if(macDate.getDay() >=7){
@@ -11,4 +12,30 @@ titolo.innerHTML = "Buona " + giorniSettimana[numGiorno-1];
     titolo.innerHTML = "Buon " + giorniSettimana[numGiorno-1];
 }
 
-data.innerHTML =macDate.getDate() + "/" + (macDate.getMonth()+1) + "/" + macDate.getFullYear();
+// funzione per la data
+data.innerHTML = macDate.getDate() + "/" + (macDate.getMonth()+1) + "/" + macDate.getFullYear();
+console.datainnerHTML
+
+// funzione per l'orario
+setInterval(aggiornamentoOra, 1000);
+function aggiornamentoOra(){
+    macDate = new Date();
+    orario.innerHTML = aggiungiZero(macDate.getHours()) + ":" + aggiungiZero(macDate.getMinutes()) + ":" + aggiungiZero(macDate.getSeconds());
+}
+
+function aggiungiZero(valore){
+    let orario = 0;
+    if(valore<=9){
+        orario = "0"+ valore
+    }else{
+        orario = valore
+    };
+
+    return orario;
+}
+
+
+
+function bottone(){
+    document.getElementById("demo").innerHTML = "Bravo kulli!!!";
+}
