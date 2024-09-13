e01c8845b3286315a83971f4dd02545a4a51957d
arianna <arianna.copa04@gmail.com>
2024-09-13 11:22:57 +0200

    creazione nuova cartella e file per progetto finale

--- Attività/Arianna Copa/Progetto intermedio/assets/script.js
+++ Attività/Arianna Copa/Progetto intermedio/assets/script.js
@@ -1,14 +1,32 @@
 const titolo = document.getElementById("titolo");
 const data = document.getElementById("data");
-const macdate = new Date();
+
+let macdate = new Date();
 const giornisettimana = ["lunedì", "martedì", "mercoledì", "giovedì", "venerdì", "sabato", "domenica"];
 const numgiorno = macdate.getDay();
 
-titolo.innerHTML = "Buon " + giornisettimana[numgiorno-1];
-data.innerHTML = macdate.getDate() + "/" + (macdate.getMonth()+1) + "/" + macdate.getFullYear();
 
-if(macdate.getDate() >=7){
+if(numgiorno >=7){
     titolo.innerHTML = "Buona " + giornisettimana[numgiorno-1];
 } else{
-    
+    titolo.innerHTML = "Buon " + giornisettimana[numgiorno-1];
+}
+
+aggiornamentoora();
+setInterval(aggiornamentoora, 100);
+
+function aggiornamentoora(){
+    macdate = new Date();
+    data.innerHTML =  aggiungizero(macdate.getHours())+ ":" + aggiungizero(macdate.getMinutes()) + ":" + aggiungizero(macdate.getSeconds());
+}
+
+function aggiungizero(valore){
+    let orario = 0;
+    if(valore<=9){
+        orario = "0" + valore
+        } else{
+            orario = valore
+        };
+        
+        return orario; 
 }
